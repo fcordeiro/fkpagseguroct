99156a039ef4a5deea8f7f184dc4c49ffc251f20
Fernando Cordeiro <fcordeiro@users.noreply.github.com>
2016-05-13 17:00:12 -0300

    Atualização de status com estoque zero
    Realizadas alterações para permitir que o status do pedido seja alterado para "pagamento aceito" mesmo que não haja estoque do produto, para lojas que trabalham com produtos sob encomendam e não desejam que o cliente seja notificado sobre o estoque. Versão incrementada para 161.5.1.

--- fkpagseguroct.php
+++ fkpagseguroct.php
@@ -18,8 +18,8 @@ class FKpagseguroct extends PaymentModule {
 
         $this->name = 'fkpagseguroct';
         $this->tab = 'payments_gateways';
-        $this->version = '161.5.0';
-        $this->author = 'FokusFirst';
+        $this->version = '161.5.1';
+        $this->author = 'FokusFirst | Fernando Cordeiro';
 
         $this->bootstrap = true;
 
@@ -71,6 +71,7 @@ class FKpagseguroct extends PaymentModule {
             Or !Configuration::updateValue('FKPAGSEGUROCT_MSG_3', $msg_3)
             Or !Configuration::updateValue('FKPAGSEGUROCT_DDD', '|11|12|13|14|15|16|17|18|19|21|22|24|27|28|31|32|33|34|35|37|38|41|42|43|44|45|46|47|48|49|51|53|54|55|61|62|63|64|65|66|67|68|69|71|73|74|75|77|79|81|82|83|84|85|86|87|88|89|91|92|93|94|95|96|97|98|99|')
             Or !Configuration::updateValue('FKPAGSEGUROCT_BOOTSTRAP', 'on')
+            Or !Configuration::updateValue('FKPAGSEGUROCT_ESTOQUE_ZERO', 'on')
             Or !Configuration::updateValue('FKPAGSEGUROCT_URL_SESSION_PRODUCAO', 'https://ws.pagseguro.uol.com.br/v2/sessions')
             Or !Configuration::updateValue('FKPAGSEGUROCT_URL_SESSION_SANDBOX', 'https://ws.sandbox.pagseguro.uol.com.br/v2/sessions')
             Or !Configuration::updateValue('FKPAGSEGUROCT_URL_TRANSACTION_PRODUCAO', 'https://ws.pagseguro.uol.com.br/v2/transactions')
@@ -508,6 +509,7 @@ class FKpagseguroct extends PaymentModule {
                 Configuration::updateValue('FKPAGSEGUROCT_STATUS_PAGO', Tools::getValue('fkpagseguroct_status_pago'));
                 Configuration::updateValue('FKPAGSEGUROCT_STATUS_CANC', Tools::getValue('fkpagseguroct_status_canc'));
                 Configuration::updateValue('FKPAGSEGUROCT_BOOTSTRAP', Tools::getValue('fkpagseguroct_bootstrap'));
+                Configuration::updateValue('FKPAGSEGUROCT_ESTOQUE_ZERO', Tools::getValue('fkpagseguroct_estoque_zero'));
 
                 break;
 
